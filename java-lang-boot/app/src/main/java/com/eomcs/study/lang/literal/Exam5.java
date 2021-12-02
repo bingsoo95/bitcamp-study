5466a5de81fe79f44112022cfcfae9b5eb3802a1
bingsoo95 <kimminsu403@naver.com>
2021-12-02 12:03:11 +0900

    '1202'

--- java-lang-boot/app/src/main/java/com/eomcs/study/lang/literal/Exam5.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/lang/literal/Exam5.java
@@ -11,12 +11,12 @@ public class Exam5 {
 
   @GetMapping("/test1")
   public String test1() {
-    return "문자: " + 'A' + '가';
+    return "문자1: " + 'A' + '가';
   }
 
   @GetMapping("/test2")
   public String test2() {
-    return "문자: " + '\u0014' + '\uac00'; //문자에 대한 유니코드 값을 지정해도 된다.
+    return "문자2: " + '\u0014' + '\uac00'; //문자에 대한 유니코드 값을 지정해도 된다.
   }
 
   @GetMapping("/test3")
@@ -28,6 +28,12 @@ public class Exam5 {
   @GetMapping("/test4")
   public String test4() {
     return "문자2: " + (char)65 + "," + (char)44032; //문자 코드를 정수 값으로 지정한다. 대신 문자 코드임을 표시해야 한다.
+    // 대신 문자를 가리키는 코드임을 표시해야 한다.
+  }
+
+  @GetMapping("/test5")
+  public String test5() {
+    return "문자2: " + '\u4eba' + '\u00a9' + '\u03c0' + '\u03a3'; // 키보드에서 입력 불가능한 특수 문자를 가리킬 때 주로 유니코드를 사용한다.
     // 대신 문자를 가리키는 코드임을 표시해야 한다.
   }
 }  
