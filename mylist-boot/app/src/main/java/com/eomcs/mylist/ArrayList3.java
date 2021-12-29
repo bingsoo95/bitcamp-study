6bfd931da1c05a2b4a581bb22c7a0e3c227e534b
bingsoo95 <kimminsu403@naver.com>
2021-12-29 15:53:24 +0900

    1229

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ArrayList3.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ArrayList3.java
@@ -2,56 +2,27 @@ package com.eomcs.mylist;
 
 public class ArrayList3 {
 
-  static Board[] boards = new Board[5];
+  static Object[] list = new Object[5];
   static int size = 0;
 
-  // 기능:
-  // - 이메일로 연락처 정보를 찾는다.
-  // - 찾은 연락처의 배열 인덱스를 리턴한다.
-  //
-  static int indexOf(String title) {
-    for (int i = 0; i < size; i++) {
-      Board board = boards[i];
-      if (board.title.equals(title)) { 
-        return i;
-      }
+  static void add(Object obj) {
+    if (size == list.length) {
+      list = grow();
     }
-    return -1;
+    list[size++] = obj;
   }
 
-  // 기능:
-  // - 배열에서 지정한 항목을 삭제한다.
-  //
-  static Board remove(int index) {
-    Board old = boards[index];
-    for (int i = index + 1; i < size; i++) {
-      boards[i - 1] = boards[i];
-    }
-    size--;
-    return old;
-  }
-
-  // 기능:
-  // - 배열의 크기를 늘린다.
-  // - 기존 배열의 값을 복사해온다.
-  //
-  static Board[] grow() {
-    Board[] arr = new Board[newLength()];
-    copy(boards, arr);
+  static Object[] grow() {
+    Object[] arr = new Object[newLength()];
+    copy(list, arr);
     return arr;
   }
 
-  // 기능:
-  // - 주어진 배열에 대해 50% 증가시킨 새 배열의 길이를 알려준다.
-  //
   static int newLength() {
-    return boards.length + (boards.length >> 1);
+    return list.length + (list.length >> 1);
   }
 
-  // 기능: 
-  // - 배열을 복사한다.
-  // 
-  static void copy(Board[] source, Board[] target) {
+  static void copy(Object[] source, Object[] target) {
     int length = source.length;
     if (target.length < source.length) {
       length = target.length;
@@ -61,49 +32,32 @@ public class ArrayList3 {
     }
   }
 
-  // 기능:
-  // - 배열에 저장된 목록만 꺼내 새 배열에 담아 리턴한다.
-  //
-  static Board[] toArray() {
-    Board[] arr = new Board[size]; 
-    for (int i = 0; i < size; i++) { 
-      arr[i] = boards[i]; 
+  static Object[] toArray() {
+    Object[] arr = new Object[size];
+    for (int i = 0; i < size; i++) {
+      arr[i] = list[i];
     }
-    return arr; 
+    return arr;
   }
 
-  // 기능:
-  // - 배열에 항목을 추가한다.
-  // - 배열이 꽉찼으면 배열의 크기를 늘린다.
-  //
-  static void add(Board board) {
-    if (size == boards.length) { 
-      boards = grow();
-    }
-    boards[size++] = board;
-  }
-
-  // 기능:
-  // - 배열의 특정 위치에 값을 변경한다.
-  // - 리턴 값: 
-  //   - 변경하기 전에 저장되어 있던 값
-  //
-  static Board set(int index, Board board) {
-    if (index < 0 || index >= size) { // 값이 저장된 위치가 무효한 인덱스라면 
+  static Object remove(int index) {
+    if (index < 0 || index >= size) { // 값이 저장된 위치가 무효한 인덱스라면
       return null;
     }
-    Board old = boards[index];
-    boards[index] = board;
+    Object old = list[index];
+    for (int i = index + 1; i < size; i++) {
+      list[i - 1] = list[i];
+    }
+    size--;
+    return old;
+  }
+
+  static Object set(int index, Object obj) {
+    if (index < 0 || index >= size) { // 값이 저장된 위치가 무효한 인덱스라면
+      return null;
+    }
+    Object old = list[index];
+    list[index] = obj;
     return old;
   }
 }
-
-
-
-
-
-
-
-
-
-
--- mylist-boot/app/src/main/java/com/eomcs/mylist/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/BoardController.java
@@ -1,56 +1,54 @@
 package com.eomcs.mylist;
 
+import java.sql.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController 
+@RestController
 public class BoardController {
 
   @RequestMapping("/board/list")
   public Object list() {
-    return ArrayList3.toArray(); 
+    return ArrayList3.toArray();
   }
 
   @RequestMapping("/board/add")
   public Object add(Board board) {
 
+    board.setCreatedDate(new Date(System.currentTimeMillis()));
     ArrayList3.add(board);
     return ArrayList3.size;
   }
 
+
   @RequestMapping("/board/get")
-  public Object get(String title) {
-    int index = ArrayList3.indexOf(title);
-    if (index == -1) {
+  public Object get(int index) {
+    if (index < 0 || index >= ArrayList3.size) {
       return "";
     }
-
-    return ArrayList3.boards[index];
+    Board board = (Board) ArrayList3.list[index];
+    board.viewCount++;
+    return board;
   }
 
   @RequestMapping("/board/update")
-  public Object update(Board board) {
-    int index = ArrayList3.indexOf(board.title);
-    if (index == -1) {
+  public Object update(int index, Board board) {
+    if (index < 0 || index >= ArrayList3.size) {
       return 0;
     }
+
+    Board old = (Board) ArrayList3.list[index];
+    board.viewCount = old.viewCount;
+    board.createdDate = old.createdDate;
 
     return ArrayList3.set(index, board) == null ? 0 : 1;
   }
 
   @RequestMapping("/board/delete")
-  public Object delete(String title) {
-    int index = ArrayList3.indexOf(title);
-    if (index == -1) {
-      return 0;
-    }
-
-    ArrayList3.remove(index);
-    return 1;
+  public Object delete(int index) {
+    //   if (index < 0 || index >= ArrayList3.size) {
+    //     return 0;
+    //   }
+    return ArrayList3.remove(index) == null ? 0 : 1;
   }
 }
-
- 
-
-
