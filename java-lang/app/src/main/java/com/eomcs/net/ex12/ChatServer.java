efcaf142bd0341f9f41f980fbd8c8a11d10770b9
bingsoo95 <kimminsu403@naver.com>
2022-02-10 14:43:01 +0900

    0210

--- java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
@@ -29,10 +29,22 @@ public class ChatServer {
     }
   }
 
+  @SuppressWarnings("unchecked")
   public void sendMessage(String message) {
+    ArrayList deleteStreams = new ArrayList();
+
     for (int i = 0; i < clientOutputStreams.size(); i++) {
       DataOutputStream out = (DataOutputStream) clientOutputStreams.get(i);
-      try {out.writeUTF(message);} catch (Exception e) {}
+      try {
+        out.writeUTF(message);
+      } catch (Exception e) {
+        System.out.println("전송 오류: " + message);
+        deleteStreams.add(out); // 무효한 출력 스트림은 삭제 명단에 등록한다.
+      }
+    }
+
+    for (Object deleteStream : deleteStreams) { // 삭제 명단에 등록된 출력 스트림을 클라이언트 목록에서 제거한다.
+      clientOutputStreams.remove(deleteStream);
     }
   }
 
@@ -52,17 +64,20 @@ public class ChatServer {
 
         clientOutputStreams.add(out);
 
-        out.writeUTF("환영합니다!");
+        String nickname = in.readUTF();
+
+        out.writeUTF(nickname + " 님 환영합니다!");
         out.flush();
 
         while (true) {
           String message = in.readUTF();
           if (message.equals("\\quit")) {
-            out.writeUTF("Goodbye!");
+            out.writeUTF("<![QUIT[]]>"); // 연결을 끊겠다는 특별한 메시지를 클라이언트에게 보낸다.
             out.flush();
+            clientOutputStreams.remove(out); // 메시지 출력 목록에서 연결이 종료된 클라이언트를 제거한다.
             break;
           }
-          sendMessage(message);
+          sendMessage(String.format("[%s] %s", nickname, message));
         }
       } catch (Exception e) {
         System.out.println("클라이언트와의 통신 오류! - " + e.getMessage());
