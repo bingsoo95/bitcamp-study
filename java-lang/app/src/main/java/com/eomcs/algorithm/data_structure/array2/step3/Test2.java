28a31b29ef3c2634bdd8f8ef3a2651f163e5f1f7
bingsoo95 <kimminsu403@naver.com>
2022-02-15 19:24:18 +0900

    0215

--- java-lang/app/src/main/java/com/eomcs/algorithm/data_structure/array2/step3/Test2.java
+++ java-lang/app/src/main/java/com/eomcs/algorithm/data_structure/array2/step3/Test2.java
@@ -1,8 +1,8 @@
-package com.eomcs.algorithm.data_structure.array2.step2;
+package com.eomcs.algorithm.data_structure.array2.step3;
 
 import com.eomcs.algorithm.data_structure.array2.Score;
 
-public class Test {
+public class Test2 {
   public static void main(String[] args) {
     ArrayList<Score> list = new ArrayList<Score>();
     list.add(new Score("홍길동", 100, 100, 100));
@@ -11,12 +11,13 @@ public class Test {
     list.add(new Score("안중근", 70, 70, 70));
     list.add(new Score("윤봉길", 80, 90, 100));
 
+    // 불편2 해결!
+    // => 목록에서 다룰 항목의 타입을 정하고, add() 메서드에서 그 타입의 값을 받도록 정의한다.
+    // => 그러면 ArrayList 를 생성할 때 지정한 타입이 아닌 경우 컴파일 오류가 발생한다.
+    //
+    //    list.add(new String("오호라!")); // 컴파일 오류!
+
     for (int i = 0; i < list.size(); i++) {
-      // 불편1 해결!
-      // => 목록을 생성할 때 목록에서 다룰 항목의 타입을 Score로 선언한다.
-      // => 그러면 get() 메서드의 리턴 타입은 Score 가 된다.
-      // => 따라서 따로 형변환 할 필요가 없다.
-      //
       Score s = list.get(i);
       System.out.printf("%s: %d, %f\n", s.name, s.sum, s.aver);
     }
