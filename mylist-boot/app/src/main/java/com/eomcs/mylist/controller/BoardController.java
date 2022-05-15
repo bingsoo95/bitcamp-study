e03e78b75598ee4dec8b479c1e41025a3f9a2b8f
bingsoo95 <kimminsu403@naver.com>
2022-05-15 14:31:08 +0900

    0515

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
@@ -80,7 +80,7 @@ public class BoardController {
       return new ResultMap().setStatus(SUCCESS);
     } else {
       return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 게시글 작성자가 아닙니다.");
-    } //
+    } 
   }
 } 
 
